72bc05b51bd8513fd499b29126e7d03f2bada332
inhere <in.798@qq.com>
2016-09-23 10:52:39 +0800

    bug fixed.update

--- services/redis/scripts/master-slave-by-conf.sh
+++ services/redis/scripts/master-slave-by-conf.sh
@@ -4,5 +4,8 @@
 # use config for add a slave instance
 #
 
-redis-server /etc/redis/6379.conf
-redis-server /etc/redis/6380.conf
+# NOTICE: must be create dir before start the redis-server
+mkdir /data/6379 /data/6380
+
+redis-server /etc/redis/6379.conf --loglevel verbose
+redis-server /etc/redis/6380.conf --loglevel verbose
