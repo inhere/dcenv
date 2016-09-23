fbb2e95d10611d0f6723771c2252d155bfa0b841
inhere <in.798@qq.com>
2016-09-23 10:52:39 +0800

    bug fixed.update

--- services/redis/scripts/single-machine-cluster.sh
+++ services/redis/scripts/single-machine-cluster.sh
@@ -2,7 +2,7 @@
 #
 # file: single-machine-sluster.sh
 #
-# single machine redis sluster
+# single machine redis cluster
 #
 
 # simple add slave redis instance by command
@@ -14,9 +14,12 @@
 # redis-server --port 7006 --daemonize yes --slaveof 127.0.0.1 7005
 
 # by config file
-redis-server /etc/redis/node-7001.conf
-redis-server /etc/redis/node-7002.conf
-redis-server /etc/redis/node-7003.conf
-redis-server /etc/redis/node-7004.conf
-redis-server /etc/redis/node-7005.conf
-redis-server /etc/redis/node-7006.conf
+# NOTICE: must be create dir before start the redis-server
+mkdir /data/7001 /data/7002 /data/7003 /data/7004 /data/7005 /data/7006
+
+redis-server /etc/redis/cluster/7001.conf
+redis-server /etc/redis/cluster/7002.conf
+redis-server /etc/redis/cluster/7003.conf
+redis-server /etc/redis/cluster/7004.conf
+redis-server /etc/redis/cluster/7005.conf
+redis-server /etc/redis/cluster/7006.conf
