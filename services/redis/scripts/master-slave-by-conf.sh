6d5a6d36481cca8cdc5582a78eaee5ae14446f73
inhere <in.798@qq.com>
2016-09-23 11:21:34 +0800

    redis service have bug. waiting debug

--- services/redis/scripts/master-slave-by-conf.sh
+++ services/redis/scripts/master-slave-by-conf.sh
@@ -4,8 +4,5 @@
 # use config for add a slave instance
 #
 
-# NOTICE: must be create dir before start the redis-server
-mkdir /data/6379 /data/6380
-
 redis-server /etc/redis/6379.conf --loglevel verbose
 redis-server /etc/redis/6380.conf --loglevel verbose
