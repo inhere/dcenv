1d0afcad07e04e4d23f0465e4b49444b53c85245
inhere <in.798@qq.com>
2016-09-25 01:04:39 +0800

    update,redis service bug fixed

--- services/redis/scripts/master-slave-by-conf.sh
+++ services/redis/scripts/master-slave-by-conf.sh
@@ -4,5 +4,26 @@
 # use config for add a slave instance
 #
 
-redis-server /etc/redis/6379.conf --loglevel verbose
-redis-server /etc/redis/6380.conf --loglevel verbose
+CONF_PATH=/etc/redis/
+BASE_PATH=/data/
+PORT_LIST=$*
+
+if [ -z "$PORT_LIST" ]; then
+    echo 'Please provide port list. like: redis-start.sh 6379 [6380 ...]'
+fi
+
+# NOTICE: must be create dir before start the redis-server
+
+for PORT in $@; do
+    [ ! -d $BASE_PATH$PORT ] && mkdir $BASE_PATH$PORT
+
+    redis-server ${CONF_PATH}${PORT}.conf
+done
+
+if [[ $?==0 ]]; then
+    echo "  Redis server [$PORT_LIST] start successful!"
+    echo "New, will run: tail -f /var/log/redis/$1.log"
+    tail -f /var/log/redis/$1.log
+else
+    echo "  Redis server [$PORT_LIST] start failure!"
+fi
